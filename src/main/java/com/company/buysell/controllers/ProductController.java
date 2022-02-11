b447705328b33be1396c177d4731c6a2c1aa017e
Aziz <30.azizbek@gmail.com>
2022-02-11 16:02:16 +0500

    Add ProductRepository. Modify ProductController, ProductService, Product, products.ftlh. End 3rd video

--- src/main/java/com/company/buysell/controllers/ProductController.java
+++ src/main/java/com/company/buysell/controllers/ProductController.java
@@ -5,10 +5,7 @@ import com.company.buysell.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,8 +14,8 @@ public class ProductController {
 
 
     @GetMapping("/")
-    public String products(Model model) {
-        model.addAttribute("products", productService.listProducts());
+    public String products(@RequestParam(name = "title", required = false) String title, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
         return "products";
     }
 
--- src/main/java/com/company/buysell/services/ProductService.java
+++ src/main/java/com/company/buysell/services/ProductService.java
@@ -1,40 +1,43 @@
 package com.company.buysell.services;
 
 import com.company.buysell.models.Product;
+import com.company.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
 
-    {
-        products.add(new Product(ID++, "Playstation 5", "Simple description", 67000, "London", "Bob"));
-        products.add(new Product(ID++, "Iphone 13", "Simple description", 80000, "Nukus", "John"));
-    }
+    @Autowired
+    private final ProductRepository productRepository;
 
-    public List<Product> listProducts() {
-        return products;
+//    {
+//        products.add(new Product(ID++, "Playstation 5", "Simple description", 67000, "London", "Bob"));
+//        products.add(new Product(ID++, "Iphone 13", "Simple description", 80000, "Nukus", "John"));
+//    }
+
+
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) {
-                return product;
-            }
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 }
