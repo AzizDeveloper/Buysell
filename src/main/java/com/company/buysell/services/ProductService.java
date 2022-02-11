01de4efbf6bbc2c24385b077919ad5adf5705162
Aziz <30.azizbek@gmail.com>
2022-02-11 13:12:22 +0500

    Add ProductService and modify Product

---
+++ src/main/java/com/company/buysell/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.company.buysell.services;
+
+import com.company.buysell.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+    private long ID = 0;
+
+    {
+        products.add(new Product(ID++, "Playstation 5", "Simple description", 67000, "London", "Bob"))
+        products.add(new Product(ID++, "Iphone 13", "Simple description", 80000, "Nukus", "John"));
+    }
+
+    public List<Product> listProducts() {
+        return products;
+    }
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+}
