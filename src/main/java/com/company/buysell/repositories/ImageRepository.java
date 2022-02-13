72b2250466fa5549fec494f0357f820096920b5a
Aziz <30.azizbek@gmail.com>
2022-02-13 20:42:07 +0500

    Add ImageRepository.java

---
+++ src/main/java/com/company/buysell/repositories/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.company.buysell.repositories;
+
+import com.company.buysell.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
